65a495490cd8b7fb6e81793082bb4c910b2f0d7d
sam-malanchuk <malanchuksam@gmail.com>
2019-06-22 13:31:12 -0400

    cleaned up create-react-app starting

---
+++ apple-nav/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        Welcome.
+      </p>
+      {/* <header className="App-header">
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
