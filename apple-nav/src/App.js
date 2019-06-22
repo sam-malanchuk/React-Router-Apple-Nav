84e474b9406976239f092b3ef17db435aad65461
sam-malanchuk <malanchuksam@gmail.com>
2019-06-22 13:36:37 -0400

    started building a static top level menu

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -4,11 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>
-        Welcome.
-      </p>
-      {/* <header className="App-header">
-      </header> */}
+      <header>
+        <a href="/">Mac</a>
+        <a href="/">iPad</a>
+        <a href="/">iPhone</a>
+        <a href="/">Watch</a>
+        <a href="/">TV</a>
+        <a href="/">Music</a>
+        <a href="/">Support</a>
+      </header>
+      <div className="bodyContainer">
+        App Body.
+      </div>
     </div>
   );
 }
