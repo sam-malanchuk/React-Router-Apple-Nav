09ef90626e237775d4ac4b18468b6c5d6e045bf5
sam-malanchuk <malanchuksam@gmail.com>
2019-06-22 14:11:04 -0400

    created the menu array and moved the TopNavigation to a new component

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,17 +1,52 @@
 import React from 'react';
 import './App.css';
+import TopNav from './components/TopNav';
+
+const data = [
+  {
+    name: "[logo]"
+  },
+  {
+    name: "Mac",
+    links: ["MacBook", "MacBook Air", "iMac", "iMac Pro", "Mac Pro", "Mac mini", "Compare", "Pro Display XDR", "Accessories", "Mojave"]
+  },
+  {
+    name: "iPad",
+    links: ["pad 1", "pad 2", "pad 3", "pad 4"]
+  },
+  {
+    name: "iPhone",
+    links: ["phone 1", "phone 2", "phone 3", "phone 4"]
+  },
+  {
+    name: "Watch",
+    links: ["watch 1", "watch 2", "watch 3", "watch 4"]
+  },
+  {
+    name: "TV",
+    links: ["TV 1", "TV 2", "TV 3", "TV 4"]
+  },
+  {
+    name: "Music",
+    links: ["Music 1", "Music 2", "Music 3", "Music 4"]
+  },
+  {
+    name: "Support",
+    links: ["s 1", "s 2", "s 3", "s 4"]
+  },
+  {
+    name: "[search]"
+  },
+  {
+    name: "[bag]"
+  }
+];
 
 function App() {
   return (
     <div className="App">
       <header>
-        <a href="/">Mac</a>
-        <a href="/">iPad</a>
-        <a href="/">iPhone</a>
-        <a href="/">Watch</a>
-        <a href="/">TV</a>
-        <a href="/">Music</a>
-        <a href="/">Support</a>
+        <TopNav />
       </header>
       <div className="bodyContainer">
         App Body.
---
+++ apple-nav/src/components/TopNav.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function TopNav() {
+    return (
+        <nav className="topNav">
+            <a href="/">[logo]</a>
+            <a href="/">Mac</a>
+            <a href="/">iPad</a>
+            <a href="/">iPhone</a>
+            <a href="/">Watch</a>
+            <a href="/">TV</a>
+            <a href="/">Music</a>
+            <a href="/">Support</a>
+            <a href="/">[search]</a>
+            <a href="/">[bag]</a>
+        </nav>
+    );
+};
+
+export default TopNav;
